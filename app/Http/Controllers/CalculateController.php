f4949c98a2e6a57161ec519bf8c195e770d509cd
Bruno <bruno2003@gmail.com>
2022-11-16 21:35:51 -0300

    add fake values and new payloads

--- app/Http/Controllers/CalculateController.php
+++ app/Http/Controllers/CalculateController.php
@@ -6,109 +6,215 @@ use Illuminate\Http\Request;
 
 class CalculateController extends Controller
 {
-    public function calculate()
+
+    protected $merchants = [];
+    protected $payment_methods = [];
+
+    public function __construct()
     {
-        $value = 200;
+        $this->fakerValues();
 
-        $merchantA_payment = 2;
-        $merchantA_payment_type = 'perc';
-        $merchantA_payout = 1;
-        $merchantA_payout_type = 'perc';
-        $calculated['merchant_A'] = $this->calculatePayment($value, $merchantA_payment_type, $merchantA_payment, $merchantA_payout_type, $merchantA_payout, "");
+    }
 
-        $merchantB_payment = 1;
-        $merchantB_payment_type = 'value';
-        $merchantB_payout = 5;
-        $merchantB_payout_type = 'perc';
-        $calculated['merchant_B'] = $this->calculatePayment($value, $merchantB_payment_type, $merchantB_payment, $merchantB_payout_type, $merchantB_payout, "");
+    public function calculate_payment(Request $request)
+    {
+        if (!isset($this->merchants[$request->merchant_id])) {
+            return response([
+                "status" => "ERROR",
+                "message" => "Merchant not found",
+            ], 421);
+        }
 
-        $merchantC_payout = 1;
-        $merchantC_payout_type = 'value';
+        if (!in_array($request->payment_method, $this->payment_methods)) {
+            return response([
+                "status" => "ERROR",
+                "message" => "Payment method not supported",
+            ], 421);
+        }
 
-        $merchantC_payment_pix = 3;
-        $merchantC_payment_pix_type = 'value';
-        $calculated['merchant_C_pix'] = $this->calculatePayment($value, $merchantC_payment_pix_type, $merchantC_payment_pix, $merchantC_payout_type, $merchantC_payout, "pix");
+        if ($request->amount <= 0) {
+            return response([
+                "status" => "ERROR",
+                "message" => "Amount must be positive and less than zero",
+            ], 421);
+        }
 
-        $merchantC_payment_boleto = 3;
-        $merchantC_payment_boleto_type = 'value';
-        $calculated['merchant_C_boleto'] = $this->calculatePayment($value, $merchantC_payment_boleto_type, $merchantC_payment_boleto, $merchantC_payout_type, $merchantC_payout, "boleto");
+        $calculated = $this->calculatePayment($request->amount, $request->payment_method, $request->merchant_id);
 
-        $merchantC_payment_ted = 3;
-        $merchantC_payment_ted_type = 'value';
-        $calculated['merchant_C_ted'] = $this->calculatePayment($value, $merchantC_payment_ted_type, $merchantC_payment_ted, $merchantC_payout_type, $merchantC_payout, "ted");
+        return response($calculated, 200);
+    }
 
-        $merchantD_payout = 1;
-        $merchantD_payout_type = 'perc';
+    public function calculate_payout(Request $request)
+    {
+        if (!isset($this->merchants[$request->merchant_id])) {
+            return response([
+                "status" => "ERROR",
+                "message" => "Merchant not found",
+            ], 421);
+        }
 
-        $merchantD_payment_pix = 2;
-        $merchantD_payment_pix_type = 'perc';
-        $calculated['merchant_D_pix'] = $this->calculatePayment($value, $merchantD_payment_pix_type, $merchantD_payment_pix, $merchantD_payout_type, $merchantD_payout, "pix");
+        if ($request->amount <= 0) {
+            return response([
+                "status" => "ERROR",
+                "message" => "Amount must be positive and less than zero",
+            ], 421);
+        }
 
-        $merchantD_payment_boleto = 4;
-        $merchantD_payment_boleto_type = 'value';
-        $calculated['merchant_D_boleto'] = $this->calculatePayment($value, $merchantD_payment_boleto_type, $merchantD_payment_boleto, $merchantD_payout_type, $merchantD_payout, "boleto");
-
-        $merchantD_payment_ted = 2;
-        $merchantD_payment_ted_type = 'perc';
-        $calculated['merchant_D_ted'] = $this->calculatePayment($value, $merchantD_payment_ted_type, $merchantD_payment_ted, $merchantD_payout_type, $merchantD_payout, "ted");
+        $calculated = $this->calculatePayout($request->amount, $request->merchant_id);
 
         return response($calculated, 200);
     }
 
     /**
      * @param float $value
-     * @param string $payment_type
-     * @param float $payment
-     * @param string $payout_type
-     * @param float $payout
-     * @param string $type_payment
+     * @param string $payment_method
+     * @param string $merchant_id
      * @return array
      */
-
-    public function calculatePayment(float $value, string $payment_type, float $payment, string $payout_type, float $payout, string $type_payment): array
+    public function calculatePayment(float $amount, string $payment_method, string $merchant_id): array
     {
-        $return_value = [];
-        $return_value['total_amount'] = $value;
-        $return_value['$type_payment'] = $type_payment;
+        $payment_commission_value = $this->merchants[$merchant_id]["payment"][$payment_method]["value"];
+        $payment_commission_type = $this->merchants[$merchant_id]["payment"][$payment_method]["type"];
 
-        if ($value == 0) {
-            return $return_value;
+        $commission = $payment_commission_value;
+        if ($payment_commission_type == "percent")
+        {
+            $commission = ($payment_commission_value/100)*$amount;
         }
 
-        if ($payment == 0) {
-            return $return_value;
-        }
+        $amount_without_commission = $amount - $commission;
 
-        if ($payment_type == 'perc') {
-            $payment_commission_value = ($payment/100)*$value;
-            $return_value['payment']['commission'] = $payment;
-            $return_value['payment']['commission_type'] = $payment_type;
-            $return_value['payment']['commission_value'] = $payment_commission_value;
-        }
-
-        if ($payout_type == 'perc') {
-            $payout_commission_value = ($payout/100)*$value;
-            $return_value['payout']['commission'] = $payout;
-            $return_value['payout']['commission_type'] = $payout_type;
-            $return_value['payout']['commission_value'] = $payout_commission_value;
-        }
-
-        if ($payment_type == 'value') {
-            $payment_commission_value = $payment;
-            $return_value['payment']['commission'] = $payment;
-            $return_value['payment']['commission_type'] = $payment_type;
-            $return_value['payment']['commission_value'] = $payment_commission_value;
-        }
-
-        if ($payout_type == 'value') {
-            $payout_commission_value = $payout;
-            $return_value['payout']['commission'] = $payout;
-            $return_value['payout']['commission_type'] = $payout_type;
-            $return_value['payout']['commission_value'] = $payout_commission_value;
-        }
-
-        $return_value['total_without_commission'] = ($value - $payout_commission_value) - $payment_commission_value;
+        $return_value['old_balance'] = $this->merchants[$merchant_id]["balance"];
+        $return_value['new_balance'] = $this->merchants[$merchant_id]["balance"] + $amount_without_commission;
+        $return_value['amount'] = $amount;
+        $return_value['commission'] = $commission;
 
         return $return_value;
+    }
+
+    /**
+     * @param float $value
+     * @param string $merchant_id
+     * @return array
+     */
+    public function calculatePayout(float $amount, string $merchant_id): array
+    {
+        $payment_commission_value = $this->merchants[$merchant_id]["payout"]["value"];
+        $payment_commission_type = $this->merchants[$merchant_id]["payout"]["type"];
+
+        $commission = $payment_commission_value;
+        if ($payment_commission_type == "percent")
+        {
+            $commission = ($payment_commission_value/100)*$amount;
+        }
+
+        $amount_without_commission = $amount - $commission;
+
+        $return_value['old_balance'] = $this->merchants[$merchant_id]["balance"];
+        $return_value['new_balance'] = $this->merchants[$merchant_id]["balance"] + $amount_without_commission;
+        $return_value['amount'] = $amount;
+        $return_value['commission'] = $commission;
+
+        return $return_value;
+    }
+
+    /**
+     * @return void
+     */
+    public function fakerValues(): void
+    {
+        $this->payment_methods = array(
+            "bank_transfer",
+            "pix",
+            "bank_slip",
+        );
+
+        $this->merchants["A"] = array(
+            "payment" => array(
+                "bank_transfer" => array(
+                    "value" => 2,
+                    "type" => "percent"
+                ),
+                "pix" => array(
+                    "value" => 2,
+                    "type" => "percent"
+                ),
+                "bank_slip" => array(
+                    "value" => 2,
+                    "type" => "percent"
+                ),
+            ),
+            "payout" => array(
+                "value" => 1,
+                "type" => "percent"
+            ),
+            "balance" => 300,
+        );
+
+        $this->merchants["B"] = array(
+            "payment" => array(
+                "bank_transfer" => array(
+                    "value" => 1,
+                    "type" => "value"
+                ),
+                "pix" => array(
+                    "value" => 1,
+                    "type" => "value"
+                ),
+                "bank_slip" => array(
+                    "value" => 1,
+                    "type" => "value"
+                ),
+            ),
+            "payout" => array(
+                "value" => 5,
+                "type" => "percent"
+            ),
+            "balance" => 350,
+        );
+
+        $this->merchants["C"] = array(
+            "payment" => array(
+                "bank_transfer" => array(
+                    "value" => 2,
+                    "type" => "value"
+                ),
+                "pix" => array(
+                    "value" => 3,
+                    "type" => "value"
+                ),
+                "bank_slip" => array(
+                    "value" => 4,
+                    "type" => "value"
+                ),
+            ),
+            "payout" => array(
+                "value" => 1,
+                "type" => "value"
+            ),
+            "balance" => 10,
+        );
+
+        $this->merchants["D"] = array(
+            "payment" => array(
+                "bank_transfer" => array(
+                    "value" => 2,
+                    "type" => "percent"
+                ),
+                "pix" => array(
+                    "value" => 2,
+                    "type" => "percent"
+                ),
+                "bank_slip" => array(
+                    "value" => 4,
+                    "type" => "value"
+                ),
+            ),
+            "payout" => array(
+                "value" => 1,
+                "type" => "percent"
+            ),
+            "balance" => 3000,
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/calculate_payment', [\App\Http\Controllers\CalculateController::class, 'calculate_payment']);
+Route::post('/calculate_payout', [\App\Http\Controllers\CalculateController::class, 'calculate_payout']);
